db91c817a92afe531c002ba0d321bb820d2514d7
liverbool <nukboon@gmail.com>
2014-01-17 01:00:31 +0700

    move comment to head block

--- src/Magice.php
+++ src/Magice.php
@@ -1,19 +1,18 @@
 <?php
+ /**
+ * @copyright   2012-2014 ツ Joyprice corporation Ltd.
+ * @license     http://www.joyprice.org/license
+ * @link        http://www.joyprice.org/themagice
+ * @author      ツ Liverbool <liverbool@joyprice.com>
+ * @version     1.0
+ * @since       1.0
+ */
+
 namespace {
 
     use Magice\Service\Builder,
-        Magice\Service\Container,
-        Magice\Mvc\Model\Entity\Exception,
-        Magice\Registry\RegistryRefererReadOnly;
+        Magice\Service\Container;
 
-    /**
-     * @copyright   2012-2014 ツ Joyprice corporation Ltd.
-     * @license     http://www.joyprice.org/license
-     * @link        http://www.joyprice.org/themagice
-     * @author      ツ Liverbool <liverbool@joyprice.com>
-     * @version     1.0
-     * @since       1.0
-     */
     class Magice
     {
         /**
