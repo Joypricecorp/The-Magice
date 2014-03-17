38b6027505b72803d099b0395dc4858a390293b9
liverbool <nukboon@gmail.com>
2014-03-17 12:45:42 +0700

    Add Validator package
    constraints NoEmpty

---
+++ src/Magice/Validator/Constraints/NoEmptyValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Magice\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+/**
+ * @author ツ Liverbool <nukboon@gmail.com>
+ * @api
+ */
+class NoEmptyValidator extends ConstraintValidator
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof NoEmpty) {
+            throw new UnexpectedTypeException($constraint, __NAMESPACE__ . '\NoEmpty');
+        }
+
+        // remove none char
+        $value = preg_replace('/\W|_/u', '', $value);
+
+        if (false === $value || (empty($value) && '0' != $value)) {
+            $this->context->addViolation($constraint->message);
+        }
+    }
+}
