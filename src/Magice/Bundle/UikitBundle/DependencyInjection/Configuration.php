b60b62aee61ee39b0e41ed495be42c2a9be484a0
liverbool <nukboon@gmail.com>
2014-03-20 20:38:46 +0700

    add twig auto_config

--- src/Magice/Bundle/UikitBundle/DependencyInjection/Configuration.php
+++ src/Magice/Bundle/UikitBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,15 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('magice_uikit');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->arrayNode('auto_configure')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->booleanNode('twig')->defaultValue(true)->end()
+                    ->end()
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
