a3d545b2a6ae025a0bc15a02180bb7a0f0804829
liverbool <nukboon@gmail.com>
2014-03-23 01:00:58 +0700

    add form abstract type

---
+++ src/Magice/Form/Type.php
@@ -0,0 +1,36 @@
+<?php
+namespace Magice\Form;
+
+use Magice\Utils\String;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+abstract class Type extends AbstractType
+{
+    public function setDefaults(OptionsResolverInterface $resolver, array $options)
+    {
+        $maps = array();
+
+        if (isset($options['data_class']) && ($class = $options['data_class'])) {
+            foreach ((new \ReflectionClass($class))->getProperties() as $p) {
+                $maps[$p->getName()] = String::underscore($p->getName());
+            }
+        }
+
+        $resolver->setDefaults(
+            array_replace(
+                array(
+                    'data_class'        => isset($class) ? $class : null,
+                    'csrf_protection'   => true,
+                    'csrf_field_name'   => '_token',
+                    // a unique key to help generate the secret token
+                    'intention'         => $this->getName(),
+                    'validation_groups' => null, //array('registration'),
+                    'error_bubbling'    => true,
+                    'error_mapping'     => $maps
+                ),
+                $options
+            )
+        );
+    }
+}
