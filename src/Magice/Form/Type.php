4a00d51d0f867882bd9443bd495ccc7d8e68f352
liverbool <nukboon@gmail.com>
2014-03-23 01:33:24 +0700

    update options params

--- src/Magice/Form/Type.php
+++ src/Magice/Form/Type.php
@@ -1,15 +1,24 @@
 <?php
 namespace Magice\Form;
 
+use Magice\Exception\Exception;
 use Magice\Utils\String;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 abstract class Type extends AbstractType
 {
-    public function setDefaults(OptionsResolverInterface $resolver, array $options)
+    public function setDefaults(OptionsResolverInterface $resolver, $options)
     {
         $maps = array();
+
+        if (is_string($options)) {
+            $options = array('data_class' => $options);
+        }
+
+        if (!is_array($options)) {
+            throw new Exception("The default options must be array.");
+        }
 
         if (
             (isset($options['auto_error_mapping']) && $options['auto_error_mapping'] !== false)
