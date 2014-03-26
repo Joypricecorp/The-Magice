734ff55513ba1ef56954444a2a9db228197578a7
liverbool <nukboon@gmail.com>
2014-03-26 19:30:41 +0700

    add support many feature
    - [if block]
    - require with array

--- src/Magice/Asset/Importer.php
+++ src/Magice/Asset/Importer.php
@@ -10,26 +10,50 @@ class Importer
     protected static $tags = array();
     protected static $params = array();
 
-    public static function import($type, $keys, $callback = null)
+    public static function import($type, $keys = null, $callback = null)
     {
-        if (is_string($keys)) {
-            $keys = (array) $keys;
+        // TODO: split manage isType with other block
+        $isType = in_array(strtolower($type), array('js', 'css'));
+
+        if ($isType) {
+            if (is_string($keys)) {
+                $keys = (array) $keys;
+            }
+        } else {
+            if (is_string($type)) {
+                $keys = (array) $type;
+            }
         }
 
         if (!isset(static::$tags[$type])) {
             self::_import($keys);
         }
 
-        $tags = array_key_exists($type, static::$tags) ? static::$tags[$type] : null;
+        if ($isType) {
+            $tags = array_key_exists($type, static::$tags) ? static::$tags[$type] : null;
 
-        if (empty($tags)) {
-            throw Exception::createMessage("Asset importer cannot found keys: %s, type: %s.", implode(',', $keys), $type);
+            if (empty($tags)) {
+                throw Exception::createMessage("Asset importer cannot found keys: %s, type: %s.", implode(',', $keys), $type);
+            }
+
+            unset(static::$tags[$type]);
+        } else {
+            $tags = static::$tags;
         }
 
-        unset(static::$tags[$type]);
+        if ($isType) {
+            return implode("\n", $tags);
+        } else {
+            return
+                (empty($tags['css']) ? '' : implode("\n", $tags['css'])) .
+                (empty($tags['js']) ? '' : implode("\n", $tags['js']));
+        }
 
-        return implode("\n", $tags);
+    }
 
+    public static function asset($keys)
+    {
+        return self::import($keys);
     }
 
     public static function script()
@@ -54,6 +78,13 @@ class Importer
         $libs  = static::$libs ? : Configuration::parse();
         $async = $libs['async'];
 
+        if (empty($libs)) {
+            throw Exception::create(
+                "Somthing wrong 'assets.json' empty. Usually json standard file not allow comment block. Have you any comment block in your assets.json?"
+            );
+        }
+
+        // @NOTE: $params use in use($params)
         $params = static::$params = array_merge(static::$params, (array) $libs['parameters']);
 
         foreach ($keys as $key) {
@@ -85,8 +116,11 @@ class Importer
             $ls = \Magice\Utils\Arrays\Paths::run($root);
             foreach ($ls as $k => $v) {
                 if (preg_match('/@/', $k)) {
-                    if (!array_key_exists($v, static::$files)) {
-                        self::find($v, $libs, static::$files);
+                    $v = is_string($v) ? ((array) $v) : $v;
+                    foreach ($v as $vv) {
+                        if (!array_key_exists($vv, static::$files)) {
+                            self::find($vv, $libs, static::$files);
+                        }
                     }
                 } else {
                     static::$files[$key . '.' . $k] = $v;
@@ -123,11 +157,20 @@ class Importer
 
     protected static function tagScript($src, $async = true)
     {
-        return sprintf('<script type="text/javascript" src="%s"%s></script>', $src, $async ? ' async' : '');
+        $start = '';
+        $end   = '';
+
+        if (preg_match('/\[if(.*)\]/', $src, $match)) {
+            $start = '<!--' . $match[0] . '>';
+            $end   = '<![endif]-->';
+            $src   = str_replace($match[0], '', $src);
+        }
+
+        return sprintf('%s<script type="text/javascript" src="%s"%s></script>%s', $start, trim($src), $async ? ' async' : '', $end);
     }
 
     protected static function tagStyle($src)
     {
-        return sprintf('<link rel="stylesheet" type="text/css" href="%s">', $src);
+        return sprintf('<link rel="stylesheet" type="text/css" href="%s">', trim($src));
     }
 }
