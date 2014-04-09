6619cdb82f96f20a4001fa38284d50377c6eca0d
liverbool <nukboon@gmail.com>
2014-04-10 05:09:44 +0700

    add ::create util method

--- src/Magice/Object/NullAbstract.php
+++ src/Magice/Object/NullAbstract.php
@@ -3,6 +3,17 @@ namespace Magice\Object;
 
 abstract class NullAbastract implements NullInterface
 {
+    /**
+     * @return static
+     */
+    public static function create()
+    {
+        return new static;
+    }
+
+    /**
+     * @return null
+     */
     public function __call()
     {
         return null;
