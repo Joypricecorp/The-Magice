ef952b15b040a933ffa120298cdc7fe13896894c
liverbool <nukboon@gmail.com>
2014-04-10 23:15:29 +0700

    add phone doctrine type

---
+++ src/Magice/Bundle/AssetBundle/DependencyInjection/Compiler/Doctrine.php
@@ -0,0 +1,34 @@
+<?php
+namespace Magice\Bundle\AssetBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class Doctrine implements CompilerPassInterface
+{
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $bd
+     *
+     * @api
+     */
+    public function process(ContainerBuilder $bd)
+    {
+        $key = 'doctrine.dbal.connection_factory.types';
+
+        $bd->setParameter(
+            $key,
+            array_merge(
+                array(
+                    'phone' => array(
+                        'class'     => 'Misd\PhoneNumberBundle\Doctrine\DBAL\Types\PhoneNumberType',
+                        'commented' => true
+                    )
+                ),
+                $bd->getParameter($key)
+            )
+        );
+
+    }
+}
--- src/Magice/Bundle/AssetBundle/MagiceAssetBundle.php
+++ src/Magice/Bundle/AssetBundle/MagiceAssetBundle.php
@@ -2,6 +2,8 @@
 
 namespace Magice\Bundle\AssetBundle;
 
+use Magice\Bundle\AssetBundle\DependencyInjection\Compiler;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\Container;
 
@@ -17,5 +19,10 @@ class MagiceAssetBundle extends Bundle
         $cn = $this->container;
         \Magice\Asset\Configuration::setMode($cn->getParameter('kernel.debug'));
         \Magice\Asset\Configuration::setConfigurationFile($cn->getParameter('kernel.root_dir') . '/../assets.json');
+    }
+
+    public function build(ContainerBuilder $bd)
+    {
+        $bd->addCompilerPass(new Compiler\Doctrine());
     }
 }
