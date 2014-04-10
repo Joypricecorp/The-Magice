2fbf71042570998bf7f0cbdfea51ba109924f8ab
liverbool <nukboon@gmail.com>
2014-04-11 06:29:42 +0700

    remove null phone object

--- src/Magice/Form/Type/Phone.php
+++ src/Magice/Form/Type/Phone.php
@@ -2,21 +2,9 @@
 namespace Magice\Form\Type;
 
 use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Magice\Form\DataTransformer\PhoneNumberToStringTransformer;
 
 class Phone extends PhoneNumberType
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->addViewTransformer(
-            new PhoneNumberToStringTransformer($options['default_region'], $options['format'])
-        );
-    }
-
     public function getName()
     {
         return 'phone';
