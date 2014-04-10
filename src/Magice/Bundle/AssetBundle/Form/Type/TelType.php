b268dc2aebb9c0e0f35a6b5410eb9f1e1284b988
liverbool <nukboon@gmail.com>
2014-04-10 19:44:21 +0700

    add form tel type

---
+++ src/Magice/Bundle/AssetBundle/Form/Type/TelType.php
@@ -0,0 +1,18 @@
+<?php
+namespace Magice\Bundle\AssetBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TelType extends AbstractType
+{
+    public function getName()
+    {
+        return 'tel';
+    }
+
+    public function getParent()
+    {
+        return 'text';
+    }
+}
---
+++ src/Magice/Bundle/AssetBundle/Twig/Field/Tel.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of MagiceAssetBundle.
+ * (c) 2014 by ツ Liverbool <nukboon@gmail.com>
+ */
+
+namespace Magice\Bundle\AssetBundle\Twig\Field;
+
+use Symfony\Component\Form\FormView;
+use Magice\Bundle\AssetBundle\Twig\Form;
+use Magice\Bundle\AssetBundle\Twig\FieldInterface;
+
+class Url extends Text
+{
+    public static function getType()
+    {
+        return 'tel';
+    }
+
+    public static function getField(Form $form, FormView $f)
+    {
+        return parent::getField($form, $f);
+    }
+}
